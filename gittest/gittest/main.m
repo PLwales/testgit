da7f6c155b404084fcfe06d1ac35f8b3de601cd2
zkyc <zkyc@zkycdeMacBook-Pro.local>
2017-10-12 09:47:01 +0800

    测试

---
+++ gittest/gittest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  gittest
+//
+//  Created by zkyc on 2017/10/12.
+//  Copyright © 2017年 zkyc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
